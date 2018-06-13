6e1e061d52c4bc913d15b3ff95f82f3798fc1d7c
Milad Vafaeifard <milad_vafaeifard@epam.com>
2018-06-13 18:19:02 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoList from './components/TodoList';
 
@@ -8,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Todo</h1>
         </header>
         <div className="container">
           <TodoList />
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class TodoList extends Component {
     count = 1;
@@ -19,15 +20,19 @@ class TodoList extends Component {
 
     render() {
         return (
-            <div>
+            <TodoListDiv>
                 <input ref={this.textInput} placeholder="What is your mind saying?"/>
                 <button onClick={() => this.Add()}>ADD</button>
                 <ul>
                     {this.state.todos.map(({id, name}) => <li key={id}>{name} </li>)}
                 </ul>
-            </div>
+            </TodoListDiv>
         );
     }
 }
+
+const TodoListDiv = styled.div `
+    padding: 20px;
+`;
 
 export default TodoList;
